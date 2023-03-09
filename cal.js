96dff6062d0466d06e15a85f3e24320cfd702734
Adeloye Olusegun Adewale <adeloyeolusegun58@gmail.com>
2023-03-09 21:24:10 +0100

    calculator

---
+++ cal.js
@@ -0,0 +1,15 @@
+function num(par) {
+    show.innerText  += par
+     }
+    
+
+     function equ() {
+      show.innerText = eval( show.innerText)
+      
+     }
+     function AC( ) {
+      show.innerText = " "
+     }
+     function del() {
+      show.innerText = show.innerText.slice(0, -1)
+     }
